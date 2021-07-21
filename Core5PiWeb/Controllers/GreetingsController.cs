1493605d54796a4815374330f8acd767c79c12ab
Nedergaard <leander.jensen@gmail.com>
2021-07-21 08:10:13 +0200

    Working sample app with manual (scp) deployment

---
+++ Core5PiWeb/Controllers/GreetingsController.cs
@@ -0,0 +1,32 @@
+﻿using Core5PiWeb.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core5PiWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GreetingsController : ControllerBase
+    {
+        private readonly ILogger<GreetingsController> _logger;
+
+        public GreetingsController(ILogger<GreetingsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Greeting Get()
+        {
+            return
+                new Greeting
+                {
+                    Message = "Hello world!",
+                };
+        }
+    }
+}
